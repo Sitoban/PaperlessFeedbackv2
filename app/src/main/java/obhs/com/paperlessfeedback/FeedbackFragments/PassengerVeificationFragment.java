11f9dd0824a428ce8a755a77b8a87b3d5e7ad7f7
sitobanbiswas@gmail.com <0203it091054>
2018-04-09 02:17:42 +0530

    PNR Validation

--- app/src/main/java/obhs/com/paperlessfeedback/FeedbackFragments/PassengerVeificationFragment.java
+++ app/src/main/java/obhs/com/paperlessfeedback/FeedbackFragments/PassengerVeificationFragment.java
@@ -24,6 +24,7 @@ import obhs.com.paperlessfeedback.DashboardActivity;
 import obhs.com.paperlessfeedback.Beans.Passenger;
 import obhs.com.paperlessfeedback.DashboardFragments.DashboardFragment;
 import obhs.com.paperlessfeedback.FeedbackActivity;
+import obhs.com.paperlessfeedback.Network.PNRStatusCheck;
 import obhs.com.paperlessfeedback.R;
 
 /**
@@ -34,6 +35,8 @@ public class PassengerVeificationFragment extends Fragment{
     View view;
     private static final String TAG = "Feedback";
     private static String otpNumber;
+    private boolean IsMobileVerified = false;
+    private boolean IsPNRVerified = false;
 //    private Coach currentCoach;
 //    private int currentSeatNumber;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -89,15 +92,17 @@ public class PassengerVeificationFragment extends Fragment{
         final Button verifyOtpButton = (Button) view.findViewById(R.id.verify_otp_button);
         final AutoCompleteTextView otpNumberField = (AutoCompleteTextView) view.findViewById(R.id.otp_number);
         final TextInputLayout textInputOtpNumberField = (TextInputLayout) view.findViewById(R.id.text_input_otp_number);
-
+        final PassengerVeificationFragment PVfragmentView = this;
 
         otpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(TAG,"send OTP");
 
+               // new PNRStatusCheck(fragmentView).execute(pnrNumberField.getText().toString());
                 String mobileNumberString = mobileNumberField.getText().toString();
                 otpNumber = generateOTP();
+                Log.d("OTP",otpNumber);
                 // SmsManager smsManager = SmsManager.getDefault();
                 // smsManager.sendTextMessage(mobileNumberString, null, otpNumber, null, null);
 
@@ -105,8 +110,9 @@ public class PassengerVeificationFragment extends Fragment{
                 mobileNumberField.setVisibility(View.GONE);
                 otpButton.setVisibility(View.GONE);
                 textInputMobileNumberField.setVisibility(View.GONE);
-                pnrNumberField.setVisibility(View.GONE);
-                textInputPnrNumberField.setVisibility(View.GONE);
+
+                //pnrNumberField.setVisibility(View.GONE);
+                //textInputPnrNumberField.setVisibility(View.GONE);
                 ////// End Hiding
 
                 /// Start Showing
@@ -123,40 +129,60 @@ public class PassengerVeificationFragment extends Fragment{
             @Override
             public void onClick(View view) {
                 String otpNumberString = otpNumberField.getText().toString();
+                IsMobileVerified = otpNumberString.equals(otpNumber);
 
+                int verificationStep = verifyOtpButton.getText() == "Verify PNR" ? 2: 1;
                 //if(otpNumberString.equals(otpNumber))
-                if(true)
+
+                if(verificationStep == 1)
                 {
-                    Toast.makeText(getActivity() , "Passenger Verification Successful", Toast.LENGTH_SHORT).show();
+                    if(IsMobileVerified)
+                    {
+                        Toast.makeText(getActivity() , "Mobile Verification Successful", Toast.LENGTH_SHORT).show();
 
 //                    FeedbackActivity activity = (FeedbackActivity)getActivity();
 //                    FeedbackFormFragment feedbackFormFragment = new FeedbackFormFragment(activity.getCurrentFeedBackType(), activity.g);
 //                    loadFragment(feedbackFormFragment);
+                        pnrNumberField.setVisibility(View.VISIBLE);
+                        textInputPnrNumberField.setVisibility(View.VISIBLE);
 
-                    //save pnr and mobile
-                    String mobileNumber = ((AutoCompleteTextView)fragmentView.findViewById(R.id.mobile_number)).getText().toString();
-                    String pnrNumber = ((AutoCompleteTextView)fragmentView.findViewById(R.id.pnr_number)).getText().toString();
-                    feedbackActivity.setCurrentPassenger(new Passenger(mobileNumber, pnrNumber));
+                        verifyOtpButton.setText("Verify PNR");
+                        otpNumberField.setVisibility(View.GONE);
+                        textInputOtpNumberField.setVisibility(View.GONE);
 
-                    loadFragment(new FeedbackFormFragment());
+                        //save pnr and mobile
+                        //String mobileNumber = ((AutoCompleteTextView)fragmentView.findViewById(R.id.mobile_number)).getText().toString();
+                        // String pnrNumber = ((AutoCompleteTextView)fragmentView.findViewById(R.id.pnr_number)).getText().toString();
+                        //feedbackActivity.setCurrentPassenger(new Passenger(mobileNumber, pnrNumber));
+
+                        //loadFragment(new FeedbackFormFragment());
+                    }
+                    else
+                    {
+                        AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
+
+                        alertDialog.setTitle("Alert Dialog");
+
+                        alertDialog.setMessage("Invalid OTP");
+                        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Write your code here to execute after dialog closed
+
+                                //Toast.makeText(getActivity() , "You clicked on OK", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                        // Showing Alert Message
+                        alertDialog.show();
+                    }
                 }
-                else
-                {
-                    AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
-
-                    alertDialog.setTitle("Alert Dialog");
-
-                    alertDialog.setMessage("Invalid OTP");
-                    alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            // Write your code here to execute after dialog closed
-
-                            //Toast.makeText(getActivity() , "You clicked on OK", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-
-                    // Showing Alert Message
-                    alertDialog.show();
+                else {
+                    //IsPNRVerified =
+                    verifyOtpButton.setEnabled(false);
+                    String mobileNumber = ((AutoCompleteTextView)fragmentView.findViewById(R.id.mobile_number)).getText().toString();
+                     String pnrNumber = ((AutoCompleteTextView)fragmentView.findViewById(R.id.pnr_number)).getText().toString();
+                    feedbackActivity.setCurrentPassenger(new Passenger(mobileNumber, pnrNumber));
+                     new PNRStatusCheck(PVfragmentView).execute(pnrNumberField.getText().toString());
                 }
 
             }
---
+++ app/src/main/java/obhs/com/paperlessfeedback/Network/PNRStatusCheck.java
@@ -0,0 +1,119 @@
+package obhs.com.paperlessfeedback.Network;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.AsyncTask;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import obhs.com.paperlessfeedback.FeedbackFragments.FeedbackFormFragment;
+import obhs.com.paperlessfeedback.FeedbackFragments.PassengerVeificationFragment;
+import obhs.com.paperlessfeedback.R;
+
+/**
+ * Created by 1018651 on 04/09/2018.
+ */
+
+
+public class PNRStatusCheck extends AsyncTask<String, Void , Boolean> {
+
+    Fragment veificationFragment;
+    public PNRStatusCheck(Fragment veificationFragment)
+    {
+        this.veificationFragment = veificationFragment;
+    }
+
+    @Override
+    protected Boolean doInBackground(String... strings) {
+        String pnrNumber = strings[0];
+        Boolean isValid = true;
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+        try {
+            Log.d("PNR", " status check");
+
+            URL url = new URL("http://api.railwayapi.com/v2/pnr-status/pnr/"+pnrNumber+"/apikey/xifwvh1pdu");
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            //connection.connect();
+
+           // connection.setDoOutput(true);
+
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("GET Response Code :: " + responseCode);
+            if (responseCode == HttpURLConnection.HTTP_OK) { // success
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        connection.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                JSONObject jsonObj = new JSONObject(response.toString());
+                String PNRResponseCode = jsonObj.getString("response_code");
+                Log.d("PNR: ", "response_code : " + PNRResponseCode);
+                if(PNRResponseCode.equals("221"))
+                {
+                    isValid = false;
+                }
+                // print result
+                System.out.println(response.toString());
+                Log.d("PNR: ", "server reply: " + response.toString());
+            } else {
+                System.out.println("GET request not worked");
+                Log.d("PNR: ", "GET request not worked");
+            }
+
+            //os.close();
+            connection.disconnect();
+        }
+        catch (Exception e)
+        {
+            Log.d("PNR Status Expection","Chud gaya pnr ka API");
+        }
+        return isValid;
+    }
+
+    @Override
+    protected void onPostExecute(Boolean isValid) {
+
+        Log.d("PNR", "isvalid: "+isValid);
+        if(isValid)
+        {
+            Toast.makeText(veificationFragment.getActivity() , "PNR Verification Successful", Toast.LENGTH_SHORT).show();
+            FragmentManager fm = veificationFragment.getFragmentManager();
+// create a FragmentTransaction to begin the transaction and replace the Fragment
+            FragmentTransaction fragmentTransaction = fm.beginTransaction();
+// replace the FrameLayout with new Fragment
+            fragmentTransaction.replace(R.id.frameLayout, new FeedbackFormFragment());
+            fragmentTransaction.commit(); // save the changes
+        }
+        else
+        {
+            final Button verifyOtpButton = (Button) veificationFragment.getView().findViewById(R.id.verify_otp_button);
+            Toast.makeText(veificationFragment.getActivity() , "PNR Verification Failed", Toast.LENGTH_SHORT).show();
+            verifyOtpButton.setEnabled(true);
+        }
+        //final Button verifyOtpButton = (Button) veificationFragment.findViewById(R.id.verify_otp_button);
+        //verifyOtpButton.setEnabled(true);
+        //super.onPostExecute(aBoolean);
+    }
+
+}
